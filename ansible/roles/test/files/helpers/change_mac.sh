88e725c64745e258531d31c45fcdf1bab75f5ab0
ignw <ignwizard@ignw.io>
2019-08-06 12:43:31 -0700

    Updated script to work with updated ifconfig output

--- ansible/roles/test/files/helpers/change_mac.sh
+++ ansible/roles/test/files/helpers/change_mac.sh
@@ -2,9 +2,10 @@
 
 for i in $(ifconfig | grep eth | cut -f 1 -d ' ')
 do
-  prefix=$(ifconfig $i | grep HWaddr | cut -c39-53)
-  suffix=$( printf "%02x" ${i##eth})
-  mac=$prefix$suffix  
-  echo $i $mac
-  ifconfig $i hw ether $mac
+  port="${i//:}"
+  prefix=$(ifconfig $port | grep ether | cut -c15-29)
+  suffix=$( printf "%02x" ${port##eth})
+  mac=$prefix$suffix
+  echo $port $mac
+  ifconfig $port hw ether $mac
 done
