4d54ba94ecbb32394ff890f3e8fa849979dba9c0
ignw <ignwizard@ignw.io>
2019-09-03 20:45:25 -0700

    fixed fetch module issues and made tweaks for pr review

--- ansible/library/blkid.py
+++ ansible/library/blkid.py
@@ -3,8 +3,8 @@ from ansible.module_utils.basic import *
 
 DOCUMENTATION = '''
 ---
-module:  sonic_install_blkid
-version_added:  "1.0"
+module:  blkid
+version_added:  "2.0"
 author: Joe Lazaro (@joeslazaro) and Joe Jacobs (@joej164)
 short_description: Parse the blkid linux command and return a dict 
 description: |
@@ -15,14 +15,17 @@ description: |
 def parse_blkid_output(text):
     """Extract a dictionary based on field names in each row
 
+    Arguments:
+        text: Text table to be parsed
+
+
+    Example of text this tool will parse:
+
     /dev/sda2: LABEL="ONIE-BOOT" UUID="e88e09a3-c6c7-48e0-8ae1-e3a67efab4bb" TYPE="ext4" PTTYPE="dos" PARTLABEL="ONIE-BOOT" PARTUUID="6db4e50c-0fa1-4c00-b853-b1f66e05ab55"
     /dev/sda3: LABEL="SONiC-OS" UUID="0737fc07-6e7e-4f31-81c9-507b08dfe6d2" TYPE="ext4" PARTLABEL="SONiC-OS" PARTUUID="354b7df3-7280-48f5-af6d-7802308f9850"
     /dev/loop0: TYPE="squashfs"
     /dev/loop1: UUID="cf9c7646-b5f6-482e-bcf5-b1523b9f43e7" TYPE="ext4"
     /dev/sda1: PARTLABEL="GRUB-BOOT" PARTUUID="730500ba-46b7-435b-b057-94afc8cd1335"
-
-    Arguments:
-        text: Text table to be parsed
 
     """
     device_attribs = {}
